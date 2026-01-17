848f15db84793966286ad7cf2826a3597ed78e11
DenizZerenli <denizzerenligulya@gmail.com>
2026-01-17 14:08:18 +0300

    my second project in english verision

---
+++ test5 english verision/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test5_english_verision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Math Exam Scores of Student Alex: ");
+            Console.WriteLine();
+
+            double exam1, exam2, exam3, exam4, result;
+
+            Console.WriteLine("Please enter the 1st exam score: ");
+            exam1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 2nd exam score: ");
+            exam2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 1st oral exam score: ");
+            exam3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 2nd oral exam score: ");
+            exam4 = double.Parse(Console.ReadLine());
+
+            result = (exam1 + exam2 + exam3 + exam4) / 4;
+
+            Console.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine("Average of student Alex: " + result);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the Math Exam Scores of Student Michel: ");
+            Console.WriteLine();
+
+            double sınav1, sınav2, sınav3, sınav4, sonuç;
+
+            Console.WriteLine("Please enter the 1st exam score: ");
+            sınav1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 2nd exam score: ");
+            sınav2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 1st oral exam score: ");
+            sınav3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the 2nd oral exam score: ");
+            sınav4 = double.Parse(Console.ReadLine());
+
+            sonuç = (sınav1 + sınav2 + sınav3 + sınav4) / 4;
+
+            Console.WriteLine();
+            Console.WriteLine("Average of student Michel: " + sonuç);
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You have entered the math averages of 2 students: ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Average of student Arthur: " + result);
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Average of student Michel: " + sonuç);
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ test5 english verision/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("test5 english verision")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test5 english verision")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("043cca44-8b03-4a34-91df-e3a06e1b1ed8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
